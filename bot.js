9b8c214bbaa32a49dc23b7410548c4c2293a2dd0
xMoha <40671043+xMoha@users.noreply.github.com>
2018-08-13 11:17:34 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+const prefix = "-";
+
+
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
